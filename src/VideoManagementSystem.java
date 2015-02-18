7d9732ea7b6fa735be2c670efe24f255275f5332
larrybarker73 <larrybarker73@gmail.com>
2015-02-17 22:15:06 -0600

    1. Renamed VideoManagementSystem to VideoManagementSystemGUI to avoid confusion. 2. Deleted main class from my GUI Package and added the GUI call to the VideoManagementSystem.java 3. Commented out the runTests function.

--- src/VideoManagementSystem.java
+++ src/VideoManagementSystem.java
@@ -1,5 +1,6 @@
 
 import java.util.ArrayList;
+import videoRentalGUI.VideoManagementSystemGUI;
 
 /*
  * VideoManagementSystem
@@ -17,6 +18,38 @@ public class VideoManagementSystem
      */
     public static void main(String[] args) 
     {
+            /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(VideoManagementSystemGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(VideoManagementSystemGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(VideoManagementSystemGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(VideoManagementSystemGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new VideoManagementSystemGUI().setVisible(true);
+            }
+        });
         // Create a new DatabaseManager to give us access to the
         // MySQL Back-End subsystem
         m_databaseManager =
@@ -29,7 +62,7 @@ public class VideoManagementSystem
         // Run some simple tests. This should be removed in the final
         // application/submission and is only meant to be run during
         // development.
-        runTests(); //TODO - remove before final submission
+        //runTests(); //TODO - remove before final submission
     }
     
     private static void runTests()
